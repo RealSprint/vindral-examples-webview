b42c13f2315345d276803b38a9885359341254a8
Lukas Robertsson <lukas.robertsson@realsprint.com>
2024-05-08 14:31:06 +0200

    Add react-native Google Cast integration with Vindral (#2)

--- react-native/vindral-webview-example/App.tsx
+++ react-native/vindral-webview-example/App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Button } from "react-native";
 import WebView, { WebViewMessageEvent } from "react-native-webview";
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
+import { CastButton } from "react-native-google-cast";
+import { useVindralCast } from "./cast";
 
 interface Message {
   type: "playback state" | "volume state" | "error";
@@ -31,36 +33,36 @@ interface ErrorMessage extends Message {
 
 const script = `
   async function run() {
-    let loops = 0
+    let loops = 0;
     while (!window.vindral) {
-        if (loops > 20) {
-          throw new Error("Could not find window.vindral")
-        }
-        await new Promise(r => setTimeout(r, 100))
-        loops++
+      if (loops > 20) {
+        throw new Error("Could not find window.vindral");
+      }
+      await new Promise(r => setTimeout(r, 100));
+      loops++;
     }
     
     window.vindral.on("playback state", (state) => window.ReactNativeWebView.postMessage(JSON.stringify({
       type: "playback state",
       state,
-    })))
+    })));
     window.vindral.on("volume state", (state) => window.ReactNativeWebView.postMessage(JSON.stringify({
       type: "volume state",
       state,
-    })))
+    })));
     window.vindral.on("error", (error) => window.ReactNativeWebView.postMessage(JSON.stringify({
       type: "error",
       error,
-    })))
+    })));
   }
-  run()
+  run();
 
   true;  // note: this is required, or you'll sometimes get silent failures
 `;
 
 export default function App() {
   const webViewRef = useRef<WebView>(null);
-  
+
   const onMessage = (event: WebViewMessageEvent) => {
     const message = JSON.parse(event.nativeEvent.data);
 
@@ -85,16 +87,20 @@ export default function App() {
     }
   };
 
+  const channelId = "vindral_demo1_ci_099ee1fa-80f3-455e-aa23-3d184e93e04f";
+  useVindralCast(channelId);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <WebView
         ref={webViewRef}
         source={{
-          uri: "https://embed.vindral.com/?channelId=vindral_demo1_ci_099ee1fa-80f3-455e-aa23-3d184e93e04f",
+          uri: `https://embed.vindral.com/?channelId=${channelId}`,
         }}
         allowsFullscreenVideo
         mediaPlaybackRequiresUserAction={false}
+        allowsInlineMediaPlayback
         javaScriptEnabled
         injectedJavaScript={script}
         onMessage={onMessage}
@@ -111,6 +117,11 @@ export default function App() {
           onPress={() =>
             webViewRef.current?.injectJavaScript("window.vindral.pause()")
           }
+        />
+        <CastButton
+          style={{
+            padding: 10,
+          }}
         />
       </View>
     </View>
